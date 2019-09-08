104e2c5774bf9b02274f5e955331e54cf943720a
sabaja <jacoposabatini76@gmail.com>
2019-09-08 23:15:26 +0200

    ADD Oakta Application

--- src/main/java/com/kafka/boot/prj/PrjApplication.java
+++ src/main/java/com/kafka/boot/prj/PrjApplication.java
@@ -2,6 +2,9 @@ package com.kafka.boot.prj;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.security.config.annotation.web.builders.HttpSecurity;
+import org.springframework.security.config.annotation.web.configuration.WebSecurityConfigurerAdapter;
 
 @SpringBootApplication
 public class PrjApplication {
@@ -10,7 +13,19 @@ public class PrjApplication {
         SpringApplication.run(PrjApplication.class, args);
     }
 
-
+    @Configuration
+    static class OktaOAuth2WebSecurityConfigurerAdapter extends WebSecurityConfigurerAdapter {
+        @Override
+        protected void configure(HttpSecurity http) throws Exception {
+            http
+                    .authorizeRequests()
+                    .anyRequest()
+                    .authenticated()
+                    .and()
+                    .oauth2ResourceServer()
+                    .jwt();
+        }
+    }
 
 }
 
